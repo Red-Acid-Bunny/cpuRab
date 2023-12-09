d0a2cc4a88789c28b3aa6c75845d4125b5d6b1c5
Red_Acid_Bunny <danilbudanov1917@gmail.com>
2023-12-09 20:36:08 +0500

    Add getters strReg and codeReg

--- BaseRab/base.h
+++ BaseRab/base.h
@@ -2,9 +2,52 @@
 #define TEST_H
 #include <cstdint>
 
-typedef struct{
+enum {
+  rv_reg_zero = 0, // Регистр специального назначения. Жеско привязан к 0.
+  rv_reg_ra,       // Return address
+  rv_reg_sp,       // Steck pointer
+  rv_reg_gp,       // Global pointer
+  rv_reg_tp,       // Thread pointer
+  rv_reg_t0,       // Temporary register 0
+  rv_reg_t1,       // Temporary register 1
+  rv_reg_t2,       // Temporary register 2
+  rv_reg_s0,       // Save register 0 / frame pointer
+  rv_reg_s1,       // Save register 1
+  rv_reg_a0,       // Function argument 0
+  rv_reg_a1,       // Function argument 1
+  rv_reg_a2,       // Function argument 2
+  rv_reg_a3,       // Function argument 3
+  rv_reg_a4,       // Function argument 4
+  rv_reg_a5,       // Function argument 5
+  rv_reg_a6,       // Function argument 6
+  rv_reg_a7,       // Function argument 7
+  rv_reg_s2,       // Save register 2
+  rv_reg_s3,       // Save register 3
+  rv_reg_s4,       // Save register 4
+  rv_reg_s5,       // Save register 5
+  rv_reg_s6,       // Save register 6
+  rv_reg_s7,       // Save register 7
+  rv_reg_s8,       // Save register 8
+  rv_reg_s9,       // Save register 9
+  rv_reg_s10,      // Save register 10
+  rv_reg_s11,      // Save register 11
+  rv_reg_t3,       // Temporary register 3
+  rv_reg_t4,       // Temporary register 4
+  rv_reg_t5,       // Temporary register 5
+  rv_reg_t6,       // Temporary register 6
+};
+
+const char rv_regs[][10] = {"zero", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
+                            "s0",   "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
+                            "a6",   "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
+                            "s8",   "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+
+typedef struct {
   uint32_t opcode;
   uint32_t rd;
-}CmdLine;
+} CmdLine;
+
+uint32_t getCodeRvReg(const char*);
+const char* getStrRvReg(uint32_t);
 
 #endif // !TEST_H
